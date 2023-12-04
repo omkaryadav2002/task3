7c8fadb1932919050171fb85858dff0aaaef4347
Omkar Yadav <152442136+omkaryadav2002@users.noreply.github.com>
2023-12-05 01:26:25 +0530

    for completion task3 for password generator in python programming

---
+++ task3.py
@@ -0,0 +1,26 @@
+#Random Password Generator Using Python
+
+#import the necessary modules
+import string
+import random
+
+#input the length of password
+length = int(input('Enter the length of password'))
+
+#Define Data
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+#combine the data 
+all = lower + upper + num + symbols
+
+#use random
+temp = random.sample(all,length)
+
+#create the password
+password = "".join(temp)
+
+#print the password
+print("password:",password)
